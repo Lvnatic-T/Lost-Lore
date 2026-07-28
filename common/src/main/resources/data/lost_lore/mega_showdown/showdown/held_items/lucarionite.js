388cbefc5593da3970281ff127da6d4587be16e8
Lvnatic <thomaslinnemann55@gmail.com>
2026-07-28 21:00:50 +0200

    Lucarionite formatting fix

--- common/src/main/resources/data/lost_lore/mega_showdown/showdown/held_items/lucarionite.js
+++ common/src/main/resources/data/lost_lore/mega_showdown/showdown/held_items/lucarionite.js
@@ -2,17 +2,17 @@
   name: "Lucarionite",
 	spritenum: 594,
 	megaStone: {
-	  "Lucario": "Lucario-Mega"
-	  "Lucario-PMD-Scarf": "Lucario-Mega"
-	  "Lucario-PSMD-Scarf": "Lucario-Mega"
-	  "Lucario-Cafe-Costume": "Lucario-Mega"
-	  "Lucario-Captain-Costume": "Lucario-Mega"
-	  "Lucario-Chef-Costume": "Lucario-Mega"
-	  "Lucario-Concert-Costume": "Lucario-Mega"
-	  "Lucario-Costume_Party-Costume": "Lucario-Mega"
-	  "Lucario-Holiday-Costume": "Lucario-Mega"
-	  "Lucario-Martial_Arts-Costume": "Lucario-Mega"
-	  "Lucario-Ruins-Costume": "Lucario-Mega"
+	  "Lucario": "Lucario-Mega",
+	  "Lucario-PMD-Scarf": "Lucario-Mega",
+	  "Lucario-PSMD-Scarf": "Lucario-Mega",
+	  "Lucario-Cafe-Costume": "Lucario-Mega",
+	  "Lucario-Captain-Costume": "Lucario-Mega",
+	  "Lucario-Chef-Costume": "Lucario-Mega",
+	  "Lucario-Concert-Costume": "Lucario-Mega",
+	  "Lucario-Costume_Party-Costume": "Lucario-Mega",
+	  "Lucario-Holiday-Costume": "Lucario-Mega",
+	  "Lucario-Martial_Arts-Costume": "Lucario-Mega",
+	  "Lucario-Ruins-Costume": "Lucario-Mega",
 	  "Lucario-Space-Costume": "Lucario-Mega"
 	},
 	itemUser: [
